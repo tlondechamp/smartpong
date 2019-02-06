cbd9c1b0d4629b5948f6ab4e9a0e2ed0eba65ad4
Thomas Londechamp <tlondechamp@multiposting.fr>
2019-02-06 18:22:13 +0100

    [v1.1.7] UX optimizations (season + player pages)

--- ui/src/app/constants.ts
+++ ui/src/app/constants.ts
@@ -1,5 +1,5 @@
-export const APP_VERSION = 'v1.1.6';
-export const DATE_FORMAT = 'yyyy-MM-dd HH:mm:ss';
+export const APP_VERSION = 'v1.1.7';
+export const DATE_FORMAT = 'yyyy-MM-dd HH:mm';
 export const NB_LAST_GAMES = 20;
 export const REFRESH_DELTA = 60;
 export const ROUTER_TIMEOUT = 2000;
